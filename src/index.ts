65392c11561a4c22777d390655fe3b97761ac191
DiscreteTom <discrete_tom@outlook.com>
2023-03-17 11:30:01 +0800

    feat: send to specific client

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,7 @@ import * as readline from "readline/promises";
 import chalk from "chalk";
 
 let clientId = 1;
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-async function questionAsync(ws: WebSocket, currentClientId: number) {
-  const answer = await rl.question(">");
-  ws.send(answer, (err) => {
-    if (err) console.error(chalk.red(err.message));
-    else console.log(chalk.blue(`@${currentClientId}< ${answer}`));
-  });
-  questionAsync(ws, currentClientId);
-}
+const clients = new Map<number, WebSocket>();
 
 const argv = yargs(process.argv.slice(2))
   .options({
@@ -37,23 +24,71 @@ console.log(`Server: ws://localhost:${argv.p}  press CTRL+C to quit`);
 
 wss.on("connection", function connection(ws, req) {
   const currentClientId = clientId;
+  clients.set(currentClientId, ws);
 
   console.log(chalk.green(`Client @${currentClientId} connected`));
   if (argv.h) console.log(`Headers @${currentClientId}: ${req.headers}`);
 
-  ws.on("error", (err) =>
-    console.error(chalk.red(`@${currentClientId}: ${err.message}`))
-  );
+  ws.on("error", (err) => {
+    console.error(chalk.red(`@${currentClientId}: ${err.message}`));
+    clients.delete(currentClientId);
+  });
 
   ws.on("message", (data) =>
-    console.log(chalk.green(`@${currentClientId}> ${data}`))
+    console.log(chalk.green(`@${currentClientId} > ${data}`))
   );
 
-  ws.on("close", () =>
-    console.log(chalk.green(`Client @${currentClientId} disconnected`))
-  );
-
-  questionAsync(ws, currentClientId);
+  ws.on("close", () => {
+    console.log(chalk.green(`Client @${currentClientId} disconnected`));
+    clients.delete(currentClientId);
+  });
 
   clientId++;
 });
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function questionAsync() {
+  const answer = await rl.question("> ");
+
+  if (answer.startsWith("@all ") || !answer.match(/^@\d+ /)) {
+    // broadcast to all clients
+    const content = answer.startsWith("@all ") ? answer.slice(5) : answer;
+    await Promise.all(
+      [...clients].map(([id, ws]) => {
+        new Promise<void>((resolve) => {
+          ws.send(content, (err) => {
+            if (err) {
+              console.error(chalk.red(`@${id}: ${err.message}`));
+              clients.delete(id);
+            }
+            resolve();
+          });
+        });
+      })
+    );
+    console.log(chalk.blue(`@all < ${content}`));
+  } else {
+    const id = answer.match(/^@(\d+) /)![1];
+    const content = answer.slice(id.length + 2); // +2 for @ and space
+    const ws = clients.get(Number(id));
+    if (ws) {
+      await new Promise<void>((resolve) => {
+        ws.send(content, (err) => {
+          if (err) {
+            console.error(chalk.red(`@${id}: ${err.message}`));
+            clients.delete(Number(id));
+          } else console.log(chalk.blue(`@${id} < ${content}`));
+          resolve();
+        });
+      });
+    }
+  }
+
+  questionAsync();
+}
+
+questionAsync();
