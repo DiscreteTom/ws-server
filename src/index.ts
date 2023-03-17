57d81832a6f813278c3d886e1b80bb0b1ec343dc
DiscreteTom <discrete_tom@outlook.com>
2023-03-17 16:57:29 +0800

    chore: optimize output, don't clear line

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import yargs from "yargs/yargs";
 import { WebSocketServer, WebSocket } from "ws";
-import * as readline from "readline/promises";
+import * as readline from "readline";
 import chalk from "chalk";
 
 let clientId = 1;
@@ -31,42 +31,20 @@ const argv = yargs(process.argv.slice(2))
   })
   .parseSync();
 
-const wss = new WebSocketServer({ port: argv.p });
-console.log(`Server: ws://localhost:${argv.p}  press CTRL+C to quit`);
-
-wss.on("connection", function connection(ws, req) {
-  const currentClientId = clientId;
-  clients.set(currentClientId, ws);
-
-  console.log(chalk.green(`\rClient @${currentClientId} connected`));
-  if (argv.h) console.log(`\rHeaders @${currentClientId}: ${req.headers}`);
-
-  ws.on("error", (err) => {
-    console.error(chalk.red(`\r@${currentClientId}: ${err.message}`));
-    clients.delete(currentClientId);
-  });
-
-  ws.on("message", (data) =>
-    console.log(chalk.green(`\r@${currentClientId} > ${data}`))
-  );
-
-  ws.on("close", () => {
-    console.log(chalk.green(`\rClient @${currentClientId} disconnected`));
-    clients.delete(currentClientId);
-  });
-
-  clientId++;
-});
-wss.on("error", (err) => console.error(chalk.red(`\rServer: ${err.message}`)));
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  prompt: "> ",
 });
+rl.prompt(true);
 
-async function questionAsync() {
-  const answer = await rl.question("> ");
+function print(msg: string) {
+  process.stdout.write("\r\u001b[2K\u001b[3D"); // clear line
+  process.stdout.write(msg + "\n");
+  rl.prompt(true);
+}
 
+rl.on("line", async (answer) => {
   if (answer.startsWith("@all ") || !answer.match(/^@\d+ /)) {
     // broadcast to all clients
     const raw = answer.startsWith("@all ") ? answer.slice(5) : answer;
@@ -77,7 +55,7 @@ async function questionAsync() {
           new Promise<void>((resolve) => {
             ws.send(content, (err) => {
               if (err) {
-                console.error(chalk.red(`\r@${id}: ${err.message}`));
+                print(chalk.red(`\r@${id}: ${err.message}`));
                 clients.delete(id);
               }
               resolve();
@@ -85,7 +63,7 @@ async function questionAsync() {
           });
         })
       );
-      console.log(chalk.blue(`\r@all < ${content}`));
+      print(chalk.blue(`\r@all < ${content}`));
     }
   } else {
     const id = answer.match(/^@(\d+) /)![1];
@@ -97,17 +75,41 @@ async function questionAsync() {
         await new Promise<void>((resolve) => {
           ws.send(content, (err) => {
             if (err) {
-              console.error(chalk.red(`\r@${id}: ${err.message}`));
+              print(chalk.red(`\r@${id}: ${err.message}`));
               clients.delete(Number(id));
-            } else console.log(chalk.blue(`\r@${id} < ${content}`));
+            } else print(chalk.blue(`\r@${id} < ${content}`));
             resolve();
           });
         });
       }
     }
   }
+});
 
-  questionAsync();
-}
+const wss = new WebSocketServer({ port: argv.p });
+print(`Server: ws://localhost:${argv.p}  press CTRL+C to quit`);
 
-questionAsync();
+wss.on("connection", function connection(ws, req) {
+  const currentClientId = clientId;
+  clients.set(currentClientId, ws);
+
+  print(chalk.green(`\rClient @${currentClientId} connected`));
+  if (argv.h) print(`\rHeaders @${currentClientId}: ${req.headers}`);
+
+  ws.on("error", (err) => {
+    print(chalk.red(`\r@${currentClientId}: ${err.message}`));
+    clients.delete(currentClientId);
+  });
+
+  ws.on("message", (data) =>
+    print(chalk.green(`\r@${currentClientId} > ${data}`))
+  );
+
+  ws.on("close", () => {
+    print(chalk.green(`\rClient @${currentClientId} disconnected`));
+    clients.delete(currentClientId);
+  });
+
+  clientId++;
+});
+wss.on("error", (err) => print(chalk.red(`\rServer: ${err.message}`)));
